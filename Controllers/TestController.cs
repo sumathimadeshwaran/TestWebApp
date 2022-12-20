ab1dd172fc70867baa8a16b9bd927bbd7cff3232
Sumathi <sumathimadeshwaran@outlook.com>
2022-12-20 02:46:07 -0800

    Snyk Test

---
+++ Controllers/TestController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+
+namespace TestWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+
+        [HttpGet(Name = "Test")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            MD5CryptoServiceProvider provider = new MD5CryptoServiceProvider();
+            TripleDESCryptoServiceProvider algorith = new TripleDESCryptoServiceProvider();
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
